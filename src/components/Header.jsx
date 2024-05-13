18c94985cffe82f9b34a8fc1d9c90edb124b614e
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-05-12 21:17:47 -0600

    Added color theme and styling to main.css, header.css and footer.css, stacked nav bar, and added profile picture to project.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 import "./header.css";
 
 export default function Header() {
   const currentPage = useLocation().pathname;
   return (
     <div id="header" className="gt-0 pt-2">
-      <h1 className="mt-3 mb-3">Phyllis Ann Lataille</h1>
+      <h1 className="mt-3 mb-3">Portfolio of Phyllis Ann Lataille</h1>
       <div id="navbuttons" className="justify-content-center">
         <ul className="navbuttons mb-2">
           <Link to="/">
@@ -39,7 +40,7 @@ export default function Header() {
                 currentPage === "/contact" ? "navbutton-active" : "navbutton"
               }
             >
-              Contact
+              Contact Me
             </li>
           </Link>
         </ul>
