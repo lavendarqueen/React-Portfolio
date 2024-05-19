a79dd9b3a66624d394a05b0d396d6386222cbdfc
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-05-19 13:50:21 -0600

    Added resume to ResumePage.jsx

--- src/Pages/ResumePage.jsx
+++ src/Pages/ResumePage.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import resumePhyllisAnnLataille from "../assets/pdf/resumePhyllisAnnLataille";
 
 export default function ResumePage() {
-  return <div></div>;
+  return (
+    <div>
+      <document id="resume" src={resumePhyllisAnnLataille} width="90%" />
+      Resume of Phyllis Ann Lataille
+    </div>
+  );
 }
