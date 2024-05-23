71565f6ba2960fae92872d82fab3f78db6fbad7e
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-05-23 12:37:21 -0600

    Updated projectData

---
+++ src/data/projectData.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    title: "Personal Blog",
+    github: "https://github.com/lavendarqueen/Phyllis-Ann-Personal-Blog",
+    image: "src/assets/images/screenshotPersonalBlog.png",
+    deployed: "https://lavendarqueen.github.io/Phyllis-Ann-Personal-Blog/",
+  },
+  {
+    title: "Note Taker",
+    github: "https://github.com/lavendarqueen/Note-Taker",
+    image: "src/assets/images/screenshotNoteTaker.png",
+    deployed: "https://note-taker-ytjo.onrender.com",
+  },
+  {
+    title: "Weather Tracker",
+    github: "https://github.com/lavendarqueen/Travelers-Weather-Tracker",
+    image: "src/assets/images/screenshotWeatherTracker.png",
+    deployed: "https://lavendarqueen.github.io/Travelers-Weather-Tracker/",
+  },
+  {
+    title: "Task Board",
+    github: "https://github.com/lavendarqueen/Third-Party-APIs-Task-Board",
+    image: "src/assets/images/screenshotTaskBoard.png",
+    deployed: "https://lavendarqueen.github.io/Third-Party-APIs-Task-Board/",
+  },
+  {
+    title: "Employee Tracker",
+    github: "https://github.com/lavendarqueen/Employee-Tracker",
+    image: "src/assets/images/screenshotEmployeeTracker.png",
+    deployed:
+      "https://github.com/lavendarqueen/Employee-Tracker/assets/159203938/3f4c1ddd-10d8-46f6-abbb-e846d1a4ee9c",
+  },
+  {
+    title: "eCommerce Back End",
+    github: "https://github.com/lavendarqueen/ORM-eCommerce-Back-End",
+    image: "src/assets/images/screenshotOrmBackEnd.png",
+    deployed:
+      "https://github.com/lavendarqueen/ORM-eCommerce-Back-End/assets/159203938/8567941e-0f2c-4370-9fcd-6af476606555",
+  },
+];
