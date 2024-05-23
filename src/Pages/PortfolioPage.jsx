43b1f4945702f94fcc7a5be2d70b12f0d96fcdcf
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-05-23 10:34:10 -0600

    Renamed screenshots and imported to PortfolioPage.jsx. deleted unused images.

--- src/Pages/PortfolioPage.jsx
+++ src/Pages/PortfolioPage.jsx
@@ -2,6 +2,12 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import projectData from "../assets/data/projectData";
 import ProjectCard from "../components/ProjectCard";
+import screenshotPersonalBlog from "../assets/images/screenshotPersonalBlog";
+import screenshotNoteTaker from "../assets/images/screenshotNoteTaker";
+import screenshotWeatherTracker from "../assets/images/screenshotWeatherTracker";
+import screenshotTaskBoard from "../assets/images/screenshotTaskBoard";
+import screenshotEmployeeTracker from "../assets/images/screenshotEmployeeTracker";
+import screenshotOrmBackEnd from "../assets/images/screenshotOrmBackEnd";
 
 export default function PortfolioPage() {
   return (
