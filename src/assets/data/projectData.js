aa3c2bd310dff615cc2537eab0904b3c5a2eeeeb
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-05-24 13:14:37 -0600

    Moved portfolio images to public folder.

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -2,38 +2,38 @@ export default [
   {
     title: "Personal Blog",
     github: "https://github.com/lavendarqueen/Phyllis-Ann-Personal-Blog",
-    image: "src/assets/images/screenshotPersonalBlog.png",
+    image: "/screenshotPersonalBlog.png",
     deployed: "https://lavendarqueen.github.io/Phyllis-Ann-Personal-Blog/",
   },
   {
     title: "Note Taker",
     github: "https://github.com/lavendarqueen/Note-Taker",
-    image: "src/assets/images/screenshotNoteTaker.png",
+    image: "/screenshotNoteTaker.png",
     deployed: "https://note-taker-ytjo.onrender.com",
   },
   {
     title: "Weather Tracker",
     github: "https://github.com/lavendarqueen/Travelers-Weather-Tracker",
-    image: "src/assets/images/screenshotWeatherTracker.png",
+    image: "/screenshotWeatherTracker.png",
     deployed: "https://lavendarqueen.github.io/Travelers-Weather-Tracker/",
   },
   {
     title: "Task Board",
     github: "https://github.com/lavendarqueen/Third-Party-APIs-Task-Board",
-    image: "src/assets/images/screenshotTaskBoard.png",
+    image: "/screenshotTaskBoard.png",
     deployed: "https://lavendarqueen.github.io/Third-Party-APIs-Task-Board/",
   },
   {
     title: "Employee Tracker",
     github: "https://github.com/lavendarqueen/Employee-Tracker",
-    image: "src/assets/images/screenshotEmployeeTracker.png",
+    image: "/screenshotEmployeeTracker.png",
     deployed:
       "https://github.com/lavendarqueen/Employee-Tracker/assets/159203938/3f4c1ddd-10d8-46f6-abbb-e846d1a4ee9c",
   },
   {
     title: "eCommerce Back End",
     github: "https://github.com/lavendarqueen/ORM-eCommerce-Back-End",
-    image: "src/assets/images/screenshotOrmBackEnd.png",
+    image: "/screenshotOrmBackEnd.png",
     deployed:
       "https://github.com/lavendarqueen/ORM-eCommerce-Back-End/assets/159203938/8567941e-0f2c-4370-9fcd-6af476606555",
   },
